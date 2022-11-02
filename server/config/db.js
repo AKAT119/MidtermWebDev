2421fdd0e41994f06b8b2fd3c803644a4b98ccce
Azin Katiraee <azinnash@yahoo.com>
2022-11-02 00:15:17 -0400

    first edit

---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+const username = "Azinnash ";
+    const password = "Xv3lbyrqdu69CUkL";
+    const cluster = "Cluster0";
+    const dbname = "book_store";
+module.exports = 
+{
+    
+   // "URI": "mongodb://127.0.0.1:27017/book_store"
+//    "URI" : `mongodb+srv://${username}:${password}@cluster0.xk6ifkg.mongodb.net/${dbname}?retryWrites=true&w=majority`,
+    "URI": "mongodb+srv://Azinnash:Xv3lbyrqdu69CUkL@cluster0.xk6ifkg.mongodb.net/contactBussiness?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+}
---
+++ server/models/book.js
@@ -0,0 +1,15 @@
+let mongoose = require('mongoose');
+//creat the model
+// create a model class
+let bookModel = mongoose.Schema({
+    name: String,
+    author: String,
+    published: String,
+    description: String,
+    price: Number
+},
+{
+    collection: "books"
+});
+
+module.exports = mongoose.model('Book', bookModel);
